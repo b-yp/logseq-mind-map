c7c9040ccaae3e76de4de8766f9bcfc84289cedc
b-yp <byapeng@qq.com>
2024-09-27 15:51:15 +0800

    feat: filter block properties

--- src/utils.ts
+++ src/utils.ts
@@ -5,10 +5,23 @@ export const getData = (trees: Array<BlockEntity>): MindMap.Data[] => {
   trees.filter((tree) => !!tree.content.trim()).forEach((tree) => {
     data.push({
       data: {
-        text: tree.content,
+        text: getContent(tree),
       },
       children: !!tree.children?.length ? getData(tree.children as BlockEntity[]) : [],
     });
   });
   return data;
+};
+
+const getContent = (block: BlockEntity) => {
+  let content = block.content.trim();
+  if (block.propertiesTextValues) {
+    const propertiesKeys = Object.keys(block.propertiesTextValues);
+    const contentArray = content.split("\n");
+    const pureContentArray = contentArray.filter((item) => {
+      return !propertiesKeys.some((key) => item.trim().startsWith(`${key}::`))
+    })
+    content = pureContentArray.join("\n");
+  }
+  return content;
 };
