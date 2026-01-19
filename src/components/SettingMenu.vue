63e08bd6731a93416b4a99511008450e3590be1b
b-yp <byapeng@qq.com>
2026-01-20 01:12:25 +0800

    fix: fix drawer ghost area
    - https://github.com/b-yp/logseq-mind-map/issues/39

--- src/components/SettingMenu.vue
+++ src/components/SettingMenu.vue
@@ -53,6 +53,27 @@
         </svg>
       </label>
     </div>
+    <div
+      class="tooltip tooltip-bottom"
+      data-tip="Esc"
+    >
+      <button class="btn btn-ghost btn-circle h-7 w-7 min-h-7" @click="$emit('close')">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2.5"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -65,6 +86,7 @@ import { storeToRefs } from "pinia";
 import { setData } from "@/main";
 
 const { locale } = useI18n();
+const emit = defineEmits(["close"]);
 
 const mindMapStore = useMindMapStore();
 const commonStore = useCommonStore();
